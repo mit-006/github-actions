1f5ab00d380c36d0603e16dfb79c6be03e1ef7a5
Mit Pathar <patharmit23@gmail.com>
2026-09-17 00:11:59 +0530

    Fix typos in portfolio-deploy.yml and app.py; streamline test_and_lint.yml workflow

--- app.py
+++ app.py
@@ -5,8 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
-    return render_templatee('index.html') --fix
-=======
-    return render_templatee('index.html')
->>>>>>> a655e3b5db808c3126aef7c1b7bf03050af180a1
+    return render_template('index.html')
